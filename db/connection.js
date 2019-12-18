45e4f9b041a918c316b6d9274721833cfaaed040
Rashad Wardhana <rashadwardhana@Rashads-MacBook-Pro.local>
2019-12-18 11:50:49 +0700

    edit host error

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'mysql://bfd3914cafdd2f:beddc215@us-cdbr-iron-east-05.cleardb.net/heroku_75c1383773f3253?reconnect=true',
+    host: 'us-cdbr-iron-east-05.cleardb.net/',
     user: 'bfd3914cafdd2f',
     password: 'beddc215',
     database: 'heroku_75c1383773f3253'
